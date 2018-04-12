ae61b31388cab8749b26ff608e72c8247e55ba30
Michael Gatewood <michael@mngatewood.com>
2018-04-12 14:50:19 -0600

    Passes one test

--- src/api/apiCalls/getHouseData.test.js
+++ src/api/apiCalls/getHouseData.test.js
@@ -1,2 +1,17 @@
 import { getHouseData } from './getHouseData';
-jest.mock(getHouseData)
+import { getSwornMembers } from './getSwornMembers';
+import { mockResponse } from '../../mockData/mockData';
+
+jest.mock('./getSwornMembers');
+
+
+it('calls fetch', () => {
+  window.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(mockResponse)
+    })
+  );
+  getHouseData();
+  expect(fetch).toHaveBeenCalled();
+});
