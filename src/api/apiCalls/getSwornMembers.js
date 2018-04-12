b7e57615ba3c4e9cb43766af7630ced5df2e081e
Michael Gatewood <michael@mngatewood.com>
2018-04-12 13:12:01 -0600

    Still working n sworn members

--- src/api/apiCalls/getSwornMembers.js
+++ src/api/apiCalls/getSwornMembers.js
@@ -1,14 +1,32 @@
-export const getSwornMembers = (houseData) => {
+export const getSwornMembers = (housesData) => {
   const urlRoot = "http://localhost:3001/api/v1/character/";
-  houseData.map( house => {
+  const updatedHousesData = housesData.map( house => ({
+    console.log(house);
     const membersArray = house.swornMembers;
-    membersArray.map(async member => {
+    const promises = membersArray.map(async member => {
       const urlQuery = member.substr(49);
       const url = `${urlRoot}${urlQuery}`;
       const response = await fetch(url);
       const memberData = await response.json();
-      console.log(memberData);
+      const memberDetails = { name: memberData.name, died: memberData.died };
+      console.log(memberDetails);
     });
+    const swornMembers = (Promise.all(promises));
+    const updatedHouse = {
+      name: house.name,
+      founded: house.founded,
+      seats: house.seats,
+      titles: house.titles,
+      coatOfArms: house.coatOfArms,
+      ancestralWeapons: house.ancestralWeapons,
+      words: house.words,
+      swornMembers: []
+    }
   });
   return houseData;
+  // return Promise.all(promises);
 }
+
+
+
+
